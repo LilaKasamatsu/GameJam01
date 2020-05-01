e2df0e77175b9e62278523a142377d3260d88dff
Daniel Schutko <danielschutko@gmail.com>
2020-05-01 11:57:57 +0200

    Unity Project
    Don't look at my code

---
+++ Unity_SwamPrototype/Assets/Code/Grid.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid 
+{
+
+    //grid stuff
+
+    private int height;
+    private int width;
+    private float cellSize;
+    private int[,] gridArray;
+
+    agent1Behavior agentScript;
+    GameObject agent;
+
+    SpawnSettings spawnerScript;
+    GameObject spawnController;
+
+    List<GridList> gridList;
+
+    //public List<GridList> gridList = new List<GridList>();
+
+
+
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        //agent = GameObject.FindGameObjectWithTag("agent1");
+        //agentScript = agent.GetComponent<agent1Behavior>();
+
+        spawnController = GameObject.Find("spawnController");
+        spawnerScript = spawnController.GetComponent<SpawnSettings>();
+
+        gridArray = new int[width, height];
+
+        // #Debug Grid width + height
+        //Debug.Log(width + " " + height);
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            
+            
+
+            for (int i = 0; i < gridArray.GetLength(1); i++)
+            {
+                // #Debug every grid width and height
+                //Debug.Log(x + " , " + i);
+                spawnerScript.CreateGridObject(x, i);
+
+                //gridList.Add(new GridList (x, i, 0, 0));
+
+                //spawnerScript.gridList.Add(new GridList(x, i, 0, 0, 0));
+
+                //SetValue
+            }
+        }
+    }
+
+
+
+
+    private Vector3 GetWorldPosition(int x, int z)
+    {
+        return new Vector3(x, z) * cellSize;
+    }
+
+    public void GetXY(Vector3 worldPosition, out int x, out int z)
+    {
+        x = Mathf.FloorToInt(worldPosition.x / cellSize);
+        z = Mathf.FloorToInt(worldPosition.z / cellSize);
+    }
+
+    public void SetValue(int x, int z, int valueStructure, int valuePoint, int valueFoundation)
+    {
+        if (x >= 0 && z >= 0 && x < width && z < height)
+        {
+            //gridArray[x, z] = value;
+
+
+           
+
+            for (int i = 0; i < spawnerScript.gridList.Count; i++)
+            {
+                if (spawnerScript.gridList[i].x == x && spawnerScript.gridList[i].z == z)
+                {
+                    spawnerScript.gridList[i] = new GridList(x, z, valueStructure, valuePoint, valueFoundation);
+                 }
+            }
+
+
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int valueStructure, int valuePoint, int valueFoundation)
+    {
+        int x, z;
+        GetXY(worldPosition, out x, out z);
+        SetValue(x, z, valueStructure, valuePoint, valueFoundation);
+    }
+
+    public GridList GetValue(int x, int z)
+    {
+        if (x >= 0 && z >= 0 && x < width && z < height)
+        {
+
+
+
+            for (int i = 0; i < spawnerScript.gridList.Count; i++)
+            {
+                if (spawnerScript.gridList[i].x == x && spawnerScript.gridList[i].z == z)
+                {
+                    return spawnerScript.gridList[i];
+                }
+            }
+
+            return new GridList(x, z, 0, 0, 0);
+            //return gridArray[x, z];
+
+
+        }
+        else
+        {
+            return new GridList(x, z, 0, 0, 0);
+        }
+    }
+
+    public GridList GetValue(Vector3 worldPosition)
+    {
+        int x, z;
+        GetXY(worldPosition, out x, out z);
+        
+        return GetValue(x, z);
+    }
+
+}
---
+++ Unity_SwamPrototype/Assets/Code/agent3Behavior.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agent3Behavior : MonoBehaviour
+{
+    [SerializeField] GameObject structure;
+    [SerializeField] GameObject marker;
+    [SerializeField] float minBuildDelay;
+    [SerializeField] float maxBuildDelay;
+    [SerializeField] int maxBuildings;
+
+    string[] radiusTags =
+     {
+                 "structure",
+                 "point",
+
+         };
+
+    private int gridSize;
+
+    private GameObject ground;
+    private List<GameObject> mainPoint;
+    private GameObject[] tagStructure;
+    private GameObject[] tagPoint;
+
+
+    public Grid grid;
+
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    bool canBuild = false;
+
+    Vector3 agentMoveLocation;
+
+
+    private mainPoint pointScript;
+    public float pointRadius;
+
+
+
+
+    GameObject spawnController;
+    private SpawnSettings spawnerScript;
+
+    private Grid gridScript;
+
+    List<GridList> gridList;
+
+    void Start()
+    {
+
+        ground = GameObject.FindGameObjectWithTag("ground");
+
+
+
+        spawnController = GameObject.Find("spawnController");
+        spawnerScript = spawnController.GetComponent<SpawnSettings>();
+
+        gridList = spawnerScript.gridList;
+        gridSize = spawnerScript.gridSize;
+
+        
+
+        tagStructure = GameObject.FindGameObjectsWithTag("structure");
+        tagPoint = GameObject.FindGameObjectsWithTag("point");
+
+        mainPoint = new List<GameObject>();
+        for (int i = 0; i <= tagStructure.Length-1; i++)
+        {
+            mainPoint.Add(tagStructure[i ]);
+        }
+        for (int i = 0; i <= tagPoint.Length-1; i++)
+        {
+            mainPoint.Add(tagPoint[i]);
+        }
+
+     
+        
+
+        StartCoroutine(MoveTimer());
+
+
+    }
+
+
+
+    void Update()
+    {
+        grid = spawnerScript.grid;
+
+
+        BuildFoundation();
+
+
+    }
+
+
+    IEnumerator MoveTimer()
+    {
+        yield return new WaitForSeconds(Random.Range(minBuildDelay, maxBuildDelay));
+
+
+        //mainPoint = GameObject.FindGameObjectsWithTag("point");
+
+        /*
+        foreach (string tag in radiusTags)
+        {
+            mainPoint = GameObject.FindGameObjectsWithTag(tag);
+
+
+        }
+        */
+
+        tagStructure = GameObject.FindGameObjectsWithTag("structure");
+        tagPoint = GameObject.FindGameObjectsWithTag("point");
+
+        mainPoint = new List<GameObject>();
+        for (int i = 0; i <= tagStructure.Length-1; i++)
+        {
+            mainPoint.Add(tagStructure[i]);
+        }
+        for (int i = 0; i <= tagPoint.Length-1; i++)
+        {
+            mainPoint.Add(tagPoint[i]);
+        }
+
+
+        GameObject closestMainPoint = GetClosestTarget(mainPoint);
+
+        pointScript = closestMainPoint.GetComponent<mainPoint>();
+
+        
+
+        agentMoveLocation = new Vector3(closestMainPoint.transform.position.x + Random.Range(-pointRadius, pointRadius), transform.position.y, closestMainPoint.transform.position.z + Random.Range(-pointRadius, pointRadius));
+
+
+        canBuild = true;
+
+
+        agent.SetDestination(agentMoveLocation);
+
+        StartCoroutine(MoveTimer());
+
+    }
+
+    GameObject GetClosestTarget(List<GameObject> target)
+    {
+        GameObject tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject t in target)
+        {
+            float dist = Vector3.Distance(t.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                tMin = t;
+                minDist = dist;
+            }
+        }
+        return tMin;
+    }
+
+    private void BuildFoundation()
+    {
+
+        if (!agent.hasPath && canBuild == true)
+        {
+            Vector3 buildLocation = new Vector3(Mathf.Round(transform.position.x / gridSize) * gridSize, structure.transform.localScale.y / 2, Mathf.Round(transform.position.z / gridSize) * gridSize);
+            canBuild = false;
+
+
+            //GridList gridValue = grid.GetValue(buildLocation);
+
+
+            for (int i = 0; i < gridList.Count; i++)
+            {
+                if (gridList[i].x == buildLocation.x && gridList[i].z == buildLocation.z && gridList[i].pointAmount <= 0)
+                {
+                    if (gridList[i].foundationAmount <= 0)
+                    {
+                        Instantiate(structure, buildLocation, Quaternion.identity);
+
+
+                        gridList[i].foundationAmount = gridList[i].foundationAmount + 1;
+                    }
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
