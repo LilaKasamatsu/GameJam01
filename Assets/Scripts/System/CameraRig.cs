c00dec8fe37bdb94a0a9302fcbd60b512cc3c5b7
Philip Hildebrandt <Philiphi@web.de>
2020-05-04 16:15:32 +0200

    [Changed] Camera Controlls
    Kamera wird nun mit mausrad zum zoomen und rechter maustaste(halten) zum drehen der map gesteuert

--- Assets/Scripts/System/CameraRig.cs
+++ Assets/Scripts/System/CameraRig.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class CameraRig : MonoBehaviour
 {
     float inputAxisMouse;
-    [SerializeField] [Range(80,500)] float Rotationspeed;
+    float inputAxisScroll;
+    [SerializeField] [Range(80,500)] float rotationspeed;
+    [SerializeField] [Range(500, 1000)] float cameraZoomSpeed;
+    [SerializeField] [Range(10, 1000)] float cameraZoomTilt;
+    Camera targetCamera;
 
+    private void Start()
+    {
+        targetCamera = Camera.main;
+    }
     private void Update()
     {
         if (Input.GetMouseButton(1))
         {
-            inputAxisMouse = Input.GetAxis("Mouse X")*Rotationspeed;
+            inputAxisMouse = Input.GetAxis("Mouse X")*rotationspeed;
 
             transform.rotation = Quaternion.AngleAxis(inputAxisMouse*Time.deltaTime, transform.up)*transform.rotation;
+        }
+        inputAxisScroll = Input.GetAxis("Mouse ScrollWheel");
+        if (inputAxisScroll > 0.02 || inputAxisScroll < -0.02)
+        {
+            Vector3 cameraMoveDirection = (targetCamera.transform.position - transform.position).normalized;
+            targetCamera.transform.position -= cameraMoveDirection * inputAxisScroll * Time.deltaTime * cameraZoomSpeed;
+            targetCamera.transform.rotation = Quaternion.AngleAxis(-inputAxisScroll * Time.deltaTime * cameraZoomTilt, targetCamera.transform.right)*targetCamera.transform.rotation;
         }
     }
 
