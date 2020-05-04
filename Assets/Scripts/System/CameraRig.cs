ee09007fbaf0475f9bbef7e2653a53c904938e16
Philip Hildebrandt <Philiphi@web.de>
2020-05-04 13:54:55 +0200

    Camera überarbeitet

---
+++ Assets/Scripts/System/CameraRig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    float inputAxisMouse;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            inputAxisMouse = Input.GetAxis("Mouse X");
+
+            transform.rotation = Quaternion.AngleAxis(inputAxisMouse*Time.deltaTime, transform.up)*transform.rotation;
+        }
+    }
+
+}
