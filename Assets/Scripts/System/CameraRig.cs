36d36c12a3dbb29bb1d104af02a7526dfcccea7d
Philip Hildebrandt <Philiphi@web.de>
2020-05-04 13:59:33 +0200

    Camera optimized

--- Assets/Scripts/System/CameraRig.cs
+++ Assets/Scripts/System/CameraRig.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class CameraRig : MonoBehaviour
 {
     float inputAxisMouse;
+    [SerializeField] [Range(80,500)] float Rotationspeed;
 
     private void Update()
     {
         if (Input.GetMouseButton(1))
         {
-            inputAxisMouse = Input.GetAxis("Mouse X");
+            inputAxisMouse = Input.GetAxis("Mouse X")*Rotationspeed;
 
             transform.rotation = Quaternion.AngleAxis(inputAxisMouse*Time.deltaTime, transform.up)*transform.rotation;
         }
