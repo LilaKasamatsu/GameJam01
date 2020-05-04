95fd6ba3866a182fedb4f1de3cf55a66f553ea91
Daniel Schutko <danielschutko@gmail.com>
2020-05-04 13:05:22 +0200

    fix3

--- Assets/Scripts/System/snapToGrid.cs
+++ Assets/Scripts/System/snapToGrid.cs
@@ -7,24 +7,17 @@ public class snapToGrid : MonoBehaviour
 
     private int gridSize;
 
-    GameObject spawnController;
     private SpawnSettings spawnerScript;
 
     void Start()
     {
 
-        spawnController = GameObject.Find("spawnController");
-        spawnerScript = spawnController.GetComponent<SpawnSettings>();
+        spawnerScript = SpawnSettings.Instance.GetComponent<SpawnSettings>();
 
         gridSize = spawnerScript.cellSize;
 
 
         transform.position = new Vector3(Mathf.Round(transform.position.x / gridSize) * gridSize, Mathf.Round(transform.position.y / (gridSize/2)) * (gridSize/2), Mathf.Round(transform.position.z / gridSize) * gridSize);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
