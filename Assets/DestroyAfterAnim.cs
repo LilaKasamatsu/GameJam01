3f942d40b43fbf09e49a45fd5f6530d2b0d175cd
Lila <kasamatsulila@gmail.com>
2020-05-11 16:44:32 +0200

    [added] vfx to foundation agent deletion

---
+++ Assets/DestroyAfterAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyAfterSeconds());
+
+    }
+
+    private IEnumerator DestroyAfterSeconds()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(this.gameObject);
+    }
+}
