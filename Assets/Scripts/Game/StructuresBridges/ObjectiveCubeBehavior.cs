601d7b7ed58267d559b4868edda1cdc7880cafed
Daniel Schutko <danielschutko@gmail.com>
2020-05-11 17:38:45 +0200

    [added] Cube objective spawn

---
+++ Assets/Scripts/Game/StructuresBridges/ObjectiveCubeBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveCubeBehavior : MonoBehaviour
+{
+    AgentStack agentStack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentStack = GridArray.Instance.agentStack;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("bridge"))
+        {
+            Destroy(this.gameObject);
+            agentStack.agentAmount += 5;
+        }
+    }
+}
