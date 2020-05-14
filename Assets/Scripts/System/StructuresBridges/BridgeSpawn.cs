2c235091cd07793b839664f5bd6f64b772a154ee
Daniel Schutko <danielschutko@gmail.com>
2020-05-14 13:06:50 +0200

    [Fixed] Bridges and Signals, Signals now on double click

--- Assets/Scripts/System/StructuresBridges/BridgeSpawn.cs
+++ Assets/Scripts/System/StructuresBridges/BridgeSpawn.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BridgeSpawn : MonoBehaviour
 {
     [SerializeField] GameObject bridge;
     [SerializeField] GameObject previewBridge;
     [SerializeField] GameObject selecter;
+    [SerializeField] GameObject vfxRay;
+    [SerializeField] GameObject vfxRadius;
 
     GameObject previewObject = null;
 
@@ -34,6 +37,9 @@ public class BridgeSpawn : MonoBehaviour
 
 
     private SignalBehavior sig;
+    private float doubleClickTime = 0.22f;
+    private float lastClickTime;
+
 
     List<StructureBehavior> selectedStructures = new List<StructureBehavior>();
     List<StructureBehavior> selectedStructuresDest = new List<StructureBehavior>();
@@ -64,7 +70,6 @@ public class BridgeSpawn : MonoBehaviour
         cellY = GridArray.Instance.cellY;
         cam = Camera.main;
     }
-
 
     public void SearchConnections(int gridX, int gridZ, Vector3 destinationPoint)
     {
@@ -124,131 +129,33 @@ public class BridgeSpawn : MonoBehaviour
 
     void Update()
     {
-        //Signal Spawning stuff
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Ray raySig = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitSig;
-            
-            if (Physics.Raycast(raySig, out hitSig) && hitSig.collider.CompareTag("structure"))
-            {
-                Vector3 hitGrid = new Vector3(Mathf.Round(hitSig.point.x / cellSize) * cellSize, Mathf.Round(hitSig.point.y / cellY) * cellY, Mathf.Round(hitSig.point.z / cellSize) * cellSize);
-
-                int arrayPosX = GridArray.Instance.NumToGrid(hitGrid.x);
-                int arrayPosZ = GridArray.Instance.NumToGrid(hitGrid.z);
-                //int arrayPosY = Mathf.RoundToInt(GridArray.Instance.gridArray[arrayPosX, arrayPosZ].sizeY) * cellY;
-
-                int arrayPosY = Mathf.RoundToInt(GridArray.Instance.gridArray[arrayPosX, arrayPosZ].structureObjects[0].transform.position.y);
-                arrayPosY += GridArray.Instance.gridArray[arrayPosX, arrayPosZ].sizeY * cellY;
-
-
-                if (GridArray.Instance.gridArray[arrayPosX, arrayPosZ].bridge > 0)
-                {
-                    Vector3 spawnPosition = new Vector3(arrayPosX * cellSize, arrayPosY, arrayPosZ * cellSize);
-
-                    GameObject signalInstance = Instantiate(sig.signalObj, spawnPosition, Quaternion.identity) as GameObject;
-
-                    SearchConnections(arrayPosX, arrayPosZ, spawnPosition);
-
-                    //signalInstance.transform.GetChild(1).transform.localScale = new Vector3(sig.signalRadius * 2, sig.signalRadius * 2, sig.signalRadius * 2);
-                    
-                    //int layer_agent = LayerMask.GetMask("Agent");
-                    //sig.FindAgents(spawnPosition, sig.signalRadius, layer_agent);
-                }
-            }            
-        }
-
+    
 
         //Bridge Spawning
         if (SpawnSettings.Instance.spawnMode == false)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
+                float timeSinceLastClick = Time.time - lastClickTime;
+                lastClickTime = Time.time;
 
-                if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("structure"))
+                SpawnBridgeOrigin();
+
+                if (timeSinceLastClick <= doubleClickTime)
                 {
-                    hitGridX = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.x);
-                    hitGridZ = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.z);
-                    hitGridY = Mathf.RoundToInt(GridArray.Instance.gridArray[hitGridX, hitGridZ].sizeY) * cellY ;
-
-
-
-                    oldX = hitGridX;
-                    oldZ = hitGridZ;
-
-
-                    if (GridArray.Instance.gridArray[hitGridX, hitGridZ].foundationAmount > 0)
-                    {
-                        GridArray.Instance.gridArray[hitGridX, hitGridZ].bridge = 1;
-
-                        startPoint = new Vector3(hitGridX * cellSize, GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].transform.position.y + hitGridY, hitGridZ * cellSize);
-                        build = true;
-
-                        //selectedStructures = new List<StructureBehavior>();
-
-                        selectedOrigin = GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].GetComponent<StructureBehavior>();
-
-                        if (selectedOrigin.GetComponent<StructureBehavior>() != null)
-                        {
-                            selectedOrigin.isSelected = true;
-
-                        }
-            
-                    }
+                    Debug.Log("double");
+                    SpawnSignal();
                 }
+
+                            
             }
         
             //Check while mouse down
             if (Input.GetMouseButton(0))
             {
-                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("structure"))
-                {
-                    hitGridX = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.x);
-                    hitGridZ = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.z);
-                    hitGridY = Mathf.RoundToInt(GridArray.Instance.gridArray[hitGridX, hitGridZ].sizeY) * cellY;
-
-
-                    if (selectedDest != null && selectedDest.GetComponent<StructureBehavior>() != null)
-                    {
-                        selectedDest.isSelected = false;
-                        selectedOrigin.isSelected = true;
-
-                    }
-
-                    //selectedStructuresDest = new List<StructureBehavior>();
-
-                    if (GridArray.Instance.gridArray[hitGridX, hitGridZ].foundationAmount > 0 )
-                    {
-
-                        endPoint = new Vector3(hitGridX * cellSize, GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].transform.position.y + hitGridY, hitGridZ * cellSize);
-
-
-                        //selectedStructuresDest.Add(GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].GetComponent<StructureBehavior>());
-                        selectedDest = GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].GetComponent<StructureBehavior>();
-
-
-                        if (!isPreview)
-                        {
-                            isPreview = true;
-                            previewObject = Instantiate(previewBridge, startPoint, Quaternion.identity) as GameObject;
-
-
-                        }
-
-                        if (selectedDest.GetComponent<StructureBehavior>() != null)
-                        {
-                            selectedDest.isSelected = true;
-
-                        }
-                   
-                    }
-                }
+                SpawnBridgeHold();
             }
+
             if (isPreview)
             {
                 float previewBridgeSize = Vector3.Distance(startPoint, endPoint);
@@ -263,73 +170,197 @@ public class BridgeSpawn : MonoBehaviour
 
             if (Input.GetMouseButtonUp(0) && build == true)
             {
-                isPreview = false;
+                spawnBridgeUp();
 
-                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
+            }
+        }
+    }
 
-                if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("structure"))
+    private void SpawnSignal()
+    {
+
+        //Signal Spawning stuff
+        Ray raySig = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitSig;
+        Debug.Log("Spawn Signal1");
+
+
+        if (Physics.Raycast(raySig, out hitSig) && hitSig.collider.CompareTag("structure"))
+        {
+            Debug.Log("Spawn Signal2");
+
+            //Vector3 hitGridSig = new Vector3(Mathf.Round(hitSig.collider.gameObject.transform.position.x/ cellSize) * cellSize, Mathf.Round(hitSig.collider.gameObject.transform.position.y / cellY) * cellY, Mathf.Round(hitSig.collider.gameObject.transform.position.z / cellSize) * cellSize);
+
+            int arrayPosX = GridArray.Instance.NumToGrid(hitSig.collider.transform.position.x);
+            int arrayPosZ = GridArray.Instance.NumToGrid(hitSig.collider.transform.position.z);
+            //int arrayPosY = Mathf.RoundToInt(GridArray.Instance.gridArray[arrayPosX, arrayPosZ].sizeY) * cellY;
+
+            int arrayPosY = Mathf.RoundToInt(GridArray.Instance.gridArray[arrayPosX, arrayPosZ].structureObjects[0].transform.position.y);
+            arrayPosY += GridArray.Instance.gridArray[arrayPosX, arrayPosZ].sizeY * cellY;
+
+
+            //GridArray.Instance.gridArray[arrayPosX, arrayPosZ].bridge > 0
+            if (selectedOrigin.isBridged)
+            {
+                Debug.Log("Spawn Signal FINAL");
+
+                Vector3 spawnPosition = new Vector3(arrayPosX * cellSize, arrayPosY, arrayPosZ * cellSize);
+
+                GameObject signalInstance = Instantiate(sig.signalObj, spawnPosition, Quaternion.identity) as GameObject;
+                Instantiate(vfxRadius, spawnPosition, Quaternion.identity);
+                //Instantiate(vfxRay, spawnPosition, Quaternion.identity);
+
+                SearchConnections(arrayPosX, arrayPosZ, spawnPosition);
+
+                //signalInstance.transform.GetChild(1).transform.localScale = new Vector3(sig.signalRadius * 2, sig.signalRadius * 2, sig.signalRadius * 2);
+
+                //int layer_agent = LayerMask.GetMask("Agent");
+                //sig.FindAgents(spawnPosition, sig.signalRadius, layer_agent);
+            }
+        }
+   
+
+    }
+
+    private void SpawnBridgeOrigin()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("structure"))
+        {
+            hitGridX = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.x);
+            hitGridZ = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.z);
+            hitGridY = Mathf.RoundToInt(GridArray.Instance.gridArray[hitGridX, hitGridZ].sizeY) * cellY;
+
+
+
+            oldX = hitGridX;
+            oldZ = hitGridZ;
+            selectedOrigin = GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].GetComponent<StructureBehavior>();
+                        
+            GridArray.Instance.gridArray[hitGridX, hitGridZ].bridge = 1;
+
+            startPoint = new Vector3(hitGridX * cellSize, GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].transform.position.y + hitGridY, hitGridZ * cellSize);
+            build = true;
+
+            //selectedStructures = new List<StructureBehavior>();
+
+
+            if (selectedOrigin.GetComponent<StructureBehavior>() != null)
+            {
+                selectedOrigin.isSelected = true;
+
+            }
+        }
+    }
+    private void SpawnBridgeHold()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("structure"))
+        {
+            hitGridX = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.x);
+            hitGridZ = GridArray.Instance.NumToGrid(hit.collider.gameObject.transform.position.z);
+            hitGridY = Mathf.RoundToInt(GridArray.Instance.gridArray[hitGridX, hitGridZ].sizeY) * cellY;
+
+
+            if (selectedDest != null && selectedDest.GetComponent<StructureBehavior>() != null)
+            {
+                selectedDest.isSelected = false;
+                selectedOrigin.isSelected = true;
+
+            }
+
+            //selectedStructuresDest = new List<StructureBehavior>();
+
+
+            if (GridArray.Instance.gridArray[hitGridX, hitGridZ].foundationAmount > 0)
+            {
+
+                endPoint = new Vector3(hitGridX * cellSize, GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].transform.position.y + hitGridY, hitGridZ * cellSize);
+
+
+                selectedDest = GridArray.Instance.gridArray[hitGridX, hitGridZ].structureObjects[0].GetComponent<StructureBehavior>();
+
+
+                if (!isPreview)
                 {
-
-
-                    bridgeSize = Vector3.Distance(startPoint, endPoint);
-                    GridArray.Instance.gridArray[hitGridX, hitGridZ].bridge = 1;
+                    isPreview = true;
+                    previewObject = Instantiate(previewBridge, startPoint, Quaternion.identity) as GameObject;
 
 
                 }
-                else
+
+                if (selectedDest.GetComponent<StructureBehavior>() != null)
                 {
-                    build = false;
-                    GridArray.Instance.gridArray[oldX, oldZ].bridge = 0;
-                }
-
-                if (build == true)
-                {
-                    if (GridArray.Instance.gridArray[hitGridX, hitGridZ].foundationAmount > 0)
-                    {
-
-
-                        //Vector3 spawnStart = new Vector3(startPoint.x, startPoint.y, startPoint.z);
-                        //Vector3 spawnEnd = new Vector3(endPoint.x, endPoint.y, endPoint.z);
-
-
-                        GameObject bridgeNew = Instantiate(bridge, startPoint, Quaternion.identity) as GameObject;
-                        bridgeNew.transform.localScale = new Vector3(bridgeNew.transform.localScale.x, bridgeNew.transform.localScale.y, bridgeSize);
-                        bridgeNew.transform.LookAt(endPoint);
-
-                        bridgeNew.GetComponent<BridgeStruct>().gridOrigin = new Vector3(startPoint.x / cellSize, startPoint.y / GridArray.Instance.cellY, startPoint.z / cellSize);
-                        bridgeNew.GetComponent<BridgeStruct>().gridEnd = new Vector3(endPoint.x / cellSize, endPoint.y / GridArray.Instance.cellY, endPoint.z / cellSize);
-
-                        GridArray.Instance.gridArray[oldX, oldZ].bridgeObjects.Add(bridgeNew);
-                        GridArray.Instance.gridArray[hitGridX, hitGridZ].bridgeObjects.Add(bridgeNew);
-
-
-
-                        build = false;
-
-
-                        //Selected Destination sets to "isBridged"
-                        if (selectedDest != null && selectedDest.GetComponent<StructureBehavior>() != null)
-                        {
-                            selectedDest.isBridged = true;
-
-                        }
-
-                        //Selected Origin sets to "isBridged"
-                        if (selectedOrigin.GetComponent<StructureBehavior>() != null)
-                        {
-                            selectedOrigin.isBridged = true;
-
-                        }
-
-                    }
+                    selectedDest.isSelected = true;
 
                 }
 
+            }
+        }
+    }
+    private void spawnBridgeUp()
+    {
+        isPreview = false;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("structure") && startPoint != endPoint)
+        {
+
+
+            bridgeSize = Vector3.Distance(startPoint, endPoint);
+            GridArray.Instance.gridArray[hitGridX, hitGridZ].bridge = 1;
+
+
+        }
+        else
+        {
+            build = false;
+            GridArray.Instance.gridArray[oldX, oldZ].bridge = 0;
+        }
+
+        if (build == true)
+        {
+            //Vector3 spawnStart = new Vector3(startPoint.x, startPoint.y, startPoint.z);
+            //Vector3 spawnEnd = new Vector3(endPoint.x, endPoint.y, endPoint.z);
+
+
+            GameObject bridgeNew = Instantiate(bridge, startPoint, Quaternion.identity) as GameObject;
+            bridgeNew.transform.localScale = new Vector3(bridgeNew.transform.localScale.x, bridgeNew.transform.localScale.y, bridgeSize);
+            bridgeNew.transform.LookAt(endPoint);
+
+            bridgeNew.GetComponent<BridgeStruct>().gridOrigin = new Vector3(startPoint.x / cellSize, startPoint.y / GridArray.Instance.cellY, startPoint.z / cellSize);
+            bridgeNew.GetComponent<BridgeStruct>().gridEnd = new Vector3(endPoint.x / cellSize, endPoint.y / GridArray.Instance.cellY, endPoint.z / cellSize);
+
+            GridArray.Instance.gridArray[oldX, oldZ].bridgeObjects.Add(bridgeNew);
+            GridArray.Instance.gridArray[hitGridX, hitGridZ].bridgeObjects.Add(bridgeNew);
+
+
+
+            build = false;
+
+
+            //Selected Destination sets to "isBridged"
+            if (selectedDest != null && selectedDest.GetComponent<StructureBehavior>() != null)
+            {
+                selectedDest.isBridged = true;
+
+            }
+
+            //Selected Origin sets to "isBridged"
+            if (selectedOrigin.GetComponent<StructureBehavior>() != null)
+            {
+                selectedOrigin.isBridged = true;
 
             }
 
         }
-
     }
+
+
 }
