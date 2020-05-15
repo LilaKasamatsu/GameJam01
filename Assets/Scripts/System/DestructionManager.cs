1fe1a1d5016dd885047c8e6eb2b950ca69ec1195
Lila <kasamatsulila@gmail.com>
2020-05-15 09:14:27 +0200

    [changed] foundation height to no clip

--- Assets/Scripts/System/DestructionManager.cs
+++ Assets/Scripts/System/DestructionManager.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
 using Random = UnityEngine.Random;
