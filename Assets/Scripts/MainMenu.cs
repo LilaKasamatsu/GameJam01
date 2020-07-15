f4b6a222ae8f9c17f9f18d0d462481ed0f4a1335
Lila <kasamatsulila@gmail.com>
2020-07-15 22:34:50 +0200

    [added] menu wip tutorial

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainPanel, creditsPanel;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HideCredits();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowCredits()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        creditsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/UIAndTutorial/Tutorial.cs
+++ Assets/UIAndTutorial/Tutorial.cs
@@ -10,8 +10,16 @@ public class Tutorial : MonoBehaviour
     [SerializeField] Button buttonStructure;
     [SerializeField] Button buttonBranch;
 
+    [Header("Panels")]
+    [SerializeField] GameObject panel01;
+    [SerializeField] GameObject panel02;
+    [SerializeField] List<GameObject> panels;
+
     private SpawnSettings spawnSettings;
 
+    private bool isCoroutineRunning;
+    private bool isTextToShow;
+    private bool allAreActive;
 
     // Start is called before the first frame update
     void Start()
@@ -28,17 +36,68 @@ public class Tutorial : MonoBehaviour
         if (!spawnSettings.firstFoundation)
         {
             buttonFoundation.transform.parent.GetComponent<Animation>().Play("ButtonWaitingForClick");
-            Debug.Log("HI");
+            if (!isCoroutineRunning)
+            {
+                StartCoroutine(WaitForText(2f));
+            }
+            if (isTextToShow)
+            {
+                panel01.GetComponent<Fade>().FadeMeIn();
+                isTextToShow = false;
+            }
+            
         }else if (!spawnSettings.firstStructure)
         {
+            panel01.GetComponent<Fade>().FadeMeOut();
+            if (!isCoroutineRunning)
+            {
+                StartCoroutine(WaitForText(2f));
+            }
+            if (isTextToShow)
+            {
+                panel02.GetComponent<Fade>().FadeMeIn();
+                isTextToShow = false;
+            }
+
             buttonStructure.gameObject.SetActive(true);
             buttonStructure.transform.parent.GetComponent<Animation>().Play("ButtonWaitingForClick");
         }
         else
         {
+            panel02.GetComponent<Fade>().FadeMeOut();
             buttonBranch.gameObject.SetActive(true);
+            allAreActive = true;
+        }
+
+        if (allAreActive && !isTutorialRunning)
+        {
+            StartCoroutine(ShowTutorialTexts());
         }
     }
 
+    [SerializeField] List<float> tutorialWaitTimer;
+    bool isTutorialRunning;
+    IEnumerator ShowTutorialTexts()
+    {
+        int counter = 0;
+        isTutorialRunning = true;
+        foreach (GameObject panel in panels)
+        {
+            panel.GetComponent<Fade>().FadeMeIn();
+            yield return new WaitForSecondsRealtime(tutorialWaitTimer[counter]);
+
+            panel.GetComponent<Fade>().FadeMeOut();
+            counter++;
+            yield return new WaitForSecondsRealtime(4f);
+        }
+    }
+
+    IEnumerator WaitForText(float time)
+    {
+        isCoroutineRunning = true;
+        yield return new WaitForSecondsRealtime(time);
+        isCoroutineRunning = false;
+        isTextToShow = true;
+    }
 
 }
